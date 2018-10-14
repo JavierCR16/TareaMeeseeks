c39b67c89e13686ceb5f39c027d9d4cf378e6adf
bryan2504 <olgaSIBAJA19557>
2018-10-14 14:10:23 -0700

    vnebe

--- funciones.c
+++ funciones.c
@@ -12,30 +12,34 @@ int instancia = 1;
 int barraTrabajo = 0;
 
 int selectDificultad(char* Mensaje){
-	int dificultad;
-	char difUser;
+	int dificultad; //entero que representa la dificultad
+	char difUser; // representa la desición del usuario
 	printf("Su respuesta: %s",Mensaje);
 	while(1){
-		printf("¿Desea seleccionar la dificultad de su tarea?(Y/N)\n Si no la establece la dificultad será aleatoria.\n\nDigite su respuesta: ");
+		printf("\n¿Desea seleccionar la dificultad de su tarea?(Y/N)\n Si no la establece la dificultad será aleatoria.\n\nDigite su respuesta: ");
 		scanf("%c",&difUser);
+		getchar();
 
 		if(difUser=='y'|difUser=='Y'){ //Si dice que sí
 			printf("Seleccione la dificultad.\n(100 es una tarea trivial. 0 es una tarea imposible)\n\nDigite su respuesta: ");
 			scanf("%d",&dificultad);
+            getchar();
 			if(0<=dificultad && dificultad<=100){//validar que esté en el rango
-				break;
+				printf("Se ha seleccionado %d como la dificultad de la tarea.\n", dificultad);
+			    break;
 			}
 			else{ // no está en el rango
-				fprintf(stderr, "Error: La dificultad ingresada es inválida.\n\n");
+				fprintf(stderr, "Error: La dificultad ingresada es inválida.\n");
 				continue;
 			}
 		}
 		else if(difUser=='n'|difUser=='N'){ // Si dice que no. La dificultad va a ser aleatoria.
 			dificultad = rand()%101;
+			printf("La dificultad de su tarea es: %d\n",dificultad);
 			break;
 		}
 		else{ //No dijo nada o no fue Y/N
-			fprintf(stderr, "Respuesta inválida. Inténtelo nuevamente...\n\n");
+			fprintf(stderr, "Respuesta inválida. Inténtelo nuevamente...\n");
 			continue;
 		}
 
