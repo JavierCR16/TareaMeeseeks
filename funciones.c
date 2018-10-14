1e31f518c42edb5d6449c65f41ee06cebbfe85a0
bryan2504 <olgaSIBAJA19557>
2018-10-14 13:46:37 -0700

    fosj

--- funciones.c
+++ funciones.c
@@ -1,16 +1,48 @@
-//
-// Created by javier on 10/14/18.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include<string.h>
 #include <pthread.h>
 #include <semaphore.h>
-#include <unistd.h>
-
 
 int nivel = 1;
 int instancia = 1;
 int barraTrabajo = 0;
+
+int selectDificultad(char* Mensaje){
+	int dificultad;
+	char difUser;
+	printf("Su respuesta: %s",Mensaje);
+	while(1){
+		printf("¿Desea seleccionar la dificultad de su tarea?(Y/N)\n Si no la establece la dificultad será aleatoria.\n\nDigite su respuesta: ");
+		scanf("%c",&difUser);
+
+		if(difUser=='y'|difUser=='Y'){ //Si dice que sí
+			printf("Seleccione la dificultad.\n(100 es una tarea trivial. 0 es una tarea imposible)\n\nDigite su respuesta: ");
+			scanf("%d",&dificultad);
+			if(0<=dificultad && dificultad<=100){//validar que esté en el rango
+				break;
+			}
+			else{ // no está en el rango
+				fprintf(stderr, "Error: La dificultad ingresada es inválida.\n\n");
+				continue;
+			}
+		}
+		else if(difUser=='n'|difUser=='N'){ // Si dice que no. La dificultad va a ser aleatoria.
+			dificultad = rand()%101;
+			break;
+		}
+		else{ //No dijo nada o no fue Y/N
+			fprintf(stderr, "Respuesta inválida. Inténtelo nuevamente...\n\n");
+			continue;
+		}
+
+	}
+
+	return dificultad;
+}
 
 
 sem_t crearCandado(){
@@ -32,10 +64,10 @@ void modificarNivelInstancia(int elevarVariable){
 
     sem_wait(&lock);
 
-        if(elevarVariable)
-            nivel++;
-        else
-            instancia++;
+    if(elevarVariable)
+        nivel++;
+    else
+        instancia++;
 
 
     sem_post(&lock);
